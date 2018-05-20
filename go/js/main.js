9590263b7b91125aba95cb9efd83d23de5e825d1
294780130 <294780130@qq.com>
2018-05-20 09:40:50 +0800

    v1.0

--- go/js/main.js
+++ go/js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 $(function(){
+//    console.log("开始执行");
     let result = "";
     $("#btn").on("click",function(){
         result = "https://www.baidu.com/s?wd=";
@@ -16,9 +17,51 @@ $(function(){
     $("#pass").on("click",function(e){
         var pass=prompt("请输入密码","");
         var password = $.md5(pass);
-        var url = "";
-        url += password;
-        url += "/index.html";
-        window.location.href = url;
+        var urll = "";
+        urll += password;
+        urll += "/test.php";
+        $.ajax({
+            url:urll,
+            async:false,
+            success:function(data){
+                alert(data);
+            },
+            error:function(){
+                alert("失败");
+            }
+        });
+
+            // window.location.href = url;
+
     });
 })
+
+window.onload = function(){
+	var i = 0 , j = 0 ;
+	var image = "" , ele="" , cla="";
+	for( i = 2 ; i < 6 ; i++ ){
+		for( j = 1 ; j < 6 ; j++ ){
+			image = "<img src='go/img_high/";
+			cla = ".pic_";
+			ele = i.toString();
+			ele += "-";
+			ele += j.toString();
+			image += ele;
+			cla += ele;
+			image += ".jpg' />";
+			$(cla).append(image);
+		}
+	}
+	for( j = 6 ; j < 9 ; j++ ){
+			image = "<img src='go/img_high/";
+			cla = ".pic_";
+			ele = "2-";
+			ele += j.toString();
+			image += ele;
+			cla += ele;
+			image += ".jpg' />";
+			$(cla).append(image);
+	}
+	$(".all").css("background-image","linear-gradient(rgba(84, 84, 84, 0), rgba(84, 84, 84, 0)),url(go/img_high/bg1.jpg)").fadeIn(3000);
+//	$("body").css("background-image","linear-gradient(rgba(84, 84, 84, 0.4), rgba(84, 84, 84, 0.4)),url(go/img_high/bg1.jpg)");
+}
